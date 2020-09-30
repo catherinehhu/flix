966beeaf95848509e437bf6b4d0da46e08d254a0
Catherine Hu <caatherinehu@gmail.com>
2020-09-30 15:42:41 -0700

    added collectionview

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Catherine Hu on 9/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
